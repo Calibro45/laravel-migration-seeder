144537524818d4b392907dc948b178efcca43e53
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-22 15:44:14 +0200

    definizione colonne

--- database/migrations/2022_04_22_131240_create_trains_table.php
+++ database/migrations/2022_04_22_131240_create_trains_table.php
@@ -14,6 +14,17 @@ class CreateTrainsTable extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
+            $table->string('azienda', 50);
+            $table->string('staione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->string('data_partenza', 15);
+            $table->string('orario_partenza', 10);
+            $table->string('orario_arrivo', 10);
+            $table->string('codice_treno', 6);
+            $table->float('prezzo_biglietto', 5,2); //999.99
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->id();
             $table->timestamps();
         });
