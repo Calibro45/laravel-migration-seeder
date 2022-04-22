0c2ae3c4a2540463244225aed8ac7c2ac08d53f5
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-22 16:41:14 +0200

    seeder

--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
@@ -9,8 +11,28 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $train = new Train();
+
+        $aziende =['Trenitalia S.p.A', 'Thello S.A.S', 'Trenord', 'Italo NTV', 'SNCF Voyage Italia'];
+        $stazioni = [
+            'Bari Centrale', 
+            'Bologna Centrale', 
+            'Firenze', 
+            'Genova P. Principe',
+            'Milano Centrale',
+            'Napoli Centrale',
+            'Palermo Centrale',
+            'Roma Termini',
+            'Torino Porta Nuova',
+            'Venezia Santa Lucia'
+        ];
+
+        // proprieta
+
+        
+
+        $train->save();
     }
 }
